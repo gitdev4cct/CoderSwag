c001fb5b73ef4100d43f89a6c44f02f8bc66ad3d
R Majernik <corvinm8@gmail.com>
2019-01-21 10:55:05 -0800

    lesson 01

--- app/src/main/java/com/corvin/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/corvin/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.corvin.coderswag
+package com.corvin.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.corvin.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
